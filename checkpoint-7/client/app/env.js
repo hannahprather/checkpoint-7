e47b7f1a40f73458fc78d3110082f7065c6b1dda
Hannah Prather <prather.hannah.m@gmail.com>
2022-03-18 14:46:23 -0600

    just starting the back

--- checkpoint-7/client/app/env.js
+++ checkpoint-7/client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-gm037770.us.auth0.com'
+export const audience = 'https://hann-dev.com'
+export const clientId = 'HSthkHgJ7hTn9tWZCpF3dIa1xjrlRMLy'
